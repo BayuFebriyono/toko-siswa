8b005340816b472964f33f1b2cefa8e4a9d33cf6
BayuFebriyono <bayu.f.p.09@gmail.com>
2021-12-30 20:26:57 +0700

    Menambah error validasi add categories

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -7,45 +7,59 @@
         <div class="card">
             <div class="card-header">
                 <h4 class="card-title ">Isi Form Dibawah Dengan Lengkap</h4>
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             </div>
             <div class="card-content">
                 <div class="card-body">
-                    <form class="form form-vertical" method="POST" action="/admin-dashboard/store-category" enctype="multipart/form-data">
+                    <form class="form form-vertical" method="POST" action="/admin-dashboard/store-category"
+                        enctype="multipart/form-data">
                         @csrf
                         <div class="form-body">
                             <div class="row">
                                 <div class="col-12">
                                     <div class="form-group has-icon-left">
                                         <label for="first-name-icon">Nama Kategori</label>
+
+                                        @error('name')
+                                            <div class="invalid-feedback">
+                                                <p>{{ $message }}</p>
+                                            </div>
+                                        @enderror
                                         <div class="position-relative">
                                             <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                                placeholder="Nama Kategori" id="first-name-icon" name="name" required>
+                                                placeholder="Nama Kategori" id="first-name-icon" name="name"
+                                                value="{{ old('name') }}" required>
                                             <div class="form-control-icon">
                                                 <i class="bi bi-tags"></i>
                                             </div>
-                                            @error('name')
-                                                <div class="invalid-feedback">
-                                                    <p>{{ $message }}</p>
-                                                </div>
-                                            @enderror
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-12">
                                     <div class="form-group has-icon-left">
                                         <label for="email-id-icon">Foto</label>
-                                        <div class="position-relative">
-                                            <input type="file" class="form-control @error('url_photo') is-invalid @enderror"
-                                                placeholder="Foto" id="email-id-icon" name="url_photo" required>
-                                            <div class="form-control-icon">
-                                                <i class="bi bi-images"></i>
-                                            </div>
-                                            @error('url_photo')
+                                        @error('url_photo')
                                             <div class="invalid-feedback">
                                                 <p>{{ $message }}</p>
                                             </div>
                                         @enderror
+                                        <div class="position-relative">
+                                            <input type="file" class="form-control @error('url_photo') is-invalid @enderror"
+                                                placeholder="Foto" id="email-id-icon" name="url_photo"
+                                                value="{{ old('url_photo') }}" required>
+                                            <div class="form-control-icon">
+                                                <i class="bi bi-images"></i>
+                                            </div>
                                         </div>
+
                                     </div>
                                 </div>
                                 <div class="col-12 d-flex justify-content-end">
